00db5f443943d031bd50de839b3ce08d6acefe6c
Dennis Cardoso <dennis.cardoso.122@gmail.com>
2016-12-11 18:03:03 -0200

    New commit my code!

--- controllers/logservice.js
+++ controllers/logservice.js
@@ -1,3 +1,9 @@
 /**
  * Created by denniscardoso on 12/11/16.
  */
+var myApp = angular.module('LogService',[]);
+
+myApp.controller('LogController', function($scope, $log){
+    $scope.$log = $log;
+    $scope.message = 'AngularJS';
+});
